66d9f3b6f6bfd05676db30d1e75b75bbc16c09c4
MrsMatiw <diegojosuemendozatoala@gmail.com>
2024-11-27 09:47:00 -0500

    COMPONENTES

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
+  constructor(private actionSheetCtrl: ActionSheetController) {}
 
-  constructor() {}
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Eliminar',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            console.log('Eliminar seleccionado');
+          },
+        },
+        {
+          text: 'Compartir',
+          icon: 'share-social',
+          handler: () => {
+            console.log('Compartir seleccionado');
+          },
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          icon: 'close',
+          handler: () => {
+            console.log('Acción cancelada');
+          },
+        },
+      ],
+    });
 
+    await actionSheet.present();
+  }
 }
